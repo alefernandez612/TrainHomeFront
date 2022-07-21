fb3476fdb5984a05152506faf238b03edcc41133
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-21 12:14:12 +0200

    Funciones básicas de users.service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,4 +1,6 @@
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {lastValueFrom} from 'rxjs';
 import {User} from '../interfaces/user.interface';
 
 @Injectable({
@@ -9,10 +11,22 @@ export class UsersService {
   private users: User[];
   private apiUrl: string;
 
-  constructor () {
+  constructor (
+    private httpClient: HttpClient
+  ) {
     this.users = [];
     this.apiUrl = 'https://home-training.herokuapp.com/api/users/';
   }
 
+  getAll(): Promise<User[]> {
+    return lastValueFrom(this.httpClient.get<User[]>(this.apiUrl));
+  }
 
+  getById(id: number): Promise<User> {
+    return lastValueFrom(this.httpClient.get<User>(this.apiUrl + id));
+  }
+
+  register(form: any): Promise<User> {
+    return lastValueFrom(this.httpClient.post<User>(this.apiUrl, form));
+  }
 }
