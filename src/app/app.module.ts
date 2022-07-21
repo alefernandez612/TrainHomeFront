6fc6b6c25b8b45b0e25d87eaa088913117345110
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-21 10:19:30 +0200

    Cambios en interfaces y componentes. Instalación de dayjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
-import {RegistroComponent} from './components/registro/registro.component';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent,
+    RegisterComponent,
     LoginComponent,
     HomeComponent,
     HeaderComponent,
--- src/app/interfaces/exercise.interface.ts
+++ src/app/interfaces/exercise.interface.ts
@@ -1,2 +1,7 @@
 export interface Exercise {
+    id: number;
+    name: string;
+    difficulty: number;
+    duration: number;
+    muscle_id: number;
 }
